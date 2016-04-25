56ab4fea147f6aac8fac52eb14619f0c4fd6c322
Dillon Hoa <dillonhoa@Dillons-MacBook-Pro.local>
2016-04-25 16:23:15 +0100

    target fixes

---
+++ FlurrySDK/FlurrySDK.h
@@ -0,0 +1,19 @@
+//
+//  FlurrySDK.h
+//  FlurrySDK
+//
+//  Created by Dillon Hoa on 25/04/2016.
+//  Copyright © 2016 Yahoo, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlurrySDK.
+FOUNDATION_EXPORT double FlurrySDKVersionNumber;
+
+//! Project version string for FlurrySDK.
+FOUNDATION_EXPORT const unsigned char FlurrySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlurrySDK/PublicHeader.h>
+
+
